ee4a64b1ba6c12265c4fffea2415eff4009f604b
Jun Yamashita <jun.yamashita@tsukubadepot.net>
2021-01-24 22:39:28 +0900

    Initial Commit

---
+++ 318142RealmCollectioView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  318142RealmCollectioView
+//
+//  Created by Jun Yamashita on 2021/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
